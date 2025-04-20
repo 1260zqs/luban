b0e1863cc1d70efaf35c06479ad672adfb1e7b31
oaliGame <oali.cloud.svr1@gmail.com>
2025-04-21 01:01:03 +0800

    publish.bat

--- publish.bat
+++ publish.bat
@@ -1,6 +1,9 @@
 @echo off
+
+set publish_dir=..\..\Excel\_make\luban
+
 pushd src
-dotnet publish Luban\Luban.csproj --configuration Release /p:DebugType=none -o ..\..\Excel\tool
+dotnet publish Luban\Luban.csproj --configuration Release /p:DebugType=none -o %publish_dir%
 popd
 
 if %errorlevel% neq 0 pause
