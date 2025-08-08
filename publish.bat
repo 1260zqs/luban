20315ba556380d7645dd800447eb82bab5c21475
oaliGame <oali.cloud.svr1@gmail.com>
2025-08-09 00:38:39 +0800

    update template

--- publish.bat
+++ publish.bat
@@ -1,6 +1,11 @@
 @echo off
 
 set publish_dir=..\..\Excel\_make\luban
+if not exist %publish_dir% (
+  echo output path dose not exist %publish_dir%
+  pause
+  goto :EOF
+)
 
 pushd src
 dotnet publish Luban\Luban.csproj --configuration Release /p:DebugType=none -o %publish_dir%
